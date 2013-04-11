a5f4d81b92b4b1d84b3bf50732a6e7b98df2186d
Michał Krasnoborski <mkrdln@gmail.com>
2013-04-11 02:18:49 +0200

    added option for showing present state

--- comtog.cpp
+++ comtog.cpp
@@ -5,14 +5,25 @@
 using namespace std;
 
 int main(int argc, char ** argv) {
-	if (argc < 4 || argc > 5) {
+	bool dry = false;
+	bool present = false;
+	bool valid_options = false;
+	
+	if (argc == 4) valid_options = true;
+	if (argc == 5) {
+		if (argv[4][0] == 's') present = true, valid_options = true;
+		if (argv[4][0] == 'd') dry = true, valid_options = true;
+	}
+	
+	if (!valid_options) {
 		cerr << "Comment Toggler" << endl;
 		cerr << endl;
 		cerr << "For each line containg pattern, toggles #-comment for next n-1 of them." << endl;
 		cerr << "Toggled lines are printed on output (changed)." << endl;
-		cerr << "Provided excessive argument, performs a dry run." << endl;
+		cerr << "Provided excessive argument, performs a dry run," << endl;
+		cerr << "or shows present state instead of changed." << endl;
 		cerr << endl;
-		cerr << "	Usage: " << argv[0] << " <file> <pattern> <n> [<dry>]" << endl;
+		cerr << "	Usage: " << argv[0] << " <file> <pattern> <n> [dry|show]" << endl;
 		cerr << endl;
 		return 1;
 	}
@@ -32,14 +43,15 @@ int main(int argc, char ** argv) {
 			if (jeszcze == 0 && s.find(argv[2]) != string::npos) jeszcze = tyle;
 			if (jeszcze) {
 				jeszcze--;
+				if (present) cerr << s << endl;
 				if (s[0] == '#') s.erase(s.begin());
 				else s = "#" + s;
-				cerr << s << endl;
+				if (!present) cerr << s << endl;
 			}
 			v.push_back(s);
 		}
 	}
-	if (argc == 4 ) {
+	if (!dry) {
 		ofstream fout(argv[1]);
 		if (!fout.is_open()) {
 			cerr << "Can't write to " << argv[1] << endl;
